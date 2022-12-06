0e30e07ac5c596f643ff8c39c7435a4297a6bab3
Andrian R <andrianreyes@gmail.com>
2022-12-07 02:34:41 +0800

    This repo is just for a code challenge purposes only. Technical recruiter want to solve this and i managed to solve it

--- App.js
+++ App.js
@@ -2,9 +2,35 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+    //Create a function that will count the 
+  //word in a string without using any built in functions
+
+  function countword(word){
+
+    let parted= [""];
+    let count = 0;
+    for(let i = 0 ;i < word.length;i++){
+      if(word[i]=== ' '){
+        count ++;
+        parted.push("")
+      }
+      else{
+        parted[parted.length -1 ] = parted[parted.length - 1] + word[i];
+      }
+    }
+    return count;
+  }
+
+
+
+  // console.log("Word  Count => " + countword("replace any words here \n") )
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <Text>The quick brown fox jumps over the lazy dogs</Text>
+      <Text>Word Count: { countword('The quick brown fox jumps over the lazy dogs')}</Text>
       <StatusBar style="auto" />
     </View>
   );
